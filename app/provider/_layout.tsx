06c99cd5a81c9b30bd6b24d8b7ed16312de6ddd4
Luis Gaspari <luisgaspari@gmail.com>
2024-11-07 17:36:11 -0300

    Finalização do fluxo de login

--- app/provider/_layout.tsx
+++ app/provider/_layout.tsx
@@ -2,9 +2,10 @@ import { Stack } from "expo-router/stack"
 
 export default function Layout() {
     return (
-        // <Stack screenOptions={{ headerShown: false }}>
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-        </Stack>
+        <>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" />
+            </Stack>
+        </>
     )
 }
